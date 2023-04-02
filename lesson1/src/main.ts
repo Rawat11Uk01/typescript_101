65b367522d77f65c89543fcf46fb6b2e12b25cb1
alind <kashishrawat308@gmail.com>
2023-04-02 23:31:15 +0530

    all lessons

--- lesson1/src/main.ts
+++ lesson1/src/main.ts
@@ -11,3 +11,25 @@
 // lesson 2
 // lesson 2
 // lesson 2
+
+// let myName: string ='kittu'
+// let age : number;
+// let marrige :boolean;
+// let money :number | string;
+// myName='rawat'
+// money=12
+
+
+// const sum = (a:number,b:number)=>{
+//     return a+b
+// }
+// let result: string|number = sum(1,5)
+// let arr :{}= {};
+
+// console.log(result)
+
+
+// lesson 3
+// lesson 3
+// lesson 3
+// Array and objects
